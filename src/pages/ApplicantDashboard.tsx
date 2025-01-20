a4795d8eeeba2cd4bb2534cab29d710b6aca32f4
Shadrach Adamu <shadrachadamuul@gmail.com>
2025-01-20 09:31:59 +0100

    added navbar-dashboard

--- src/pages/ApplicantDashboard.tsx
+++ src/pages/ApplicantDashboard.tsx
@@ -1,6 +1,9 @@
+import ApplicantNavBar from "../components/layouts/ApplicantNavBar.tsx";
+
 export const ApplicantDashboard = () => {
     return (
         <>
+            <ApplicantNavBar/>
         <h1>Dashbaord</h1>
         </>
     );
