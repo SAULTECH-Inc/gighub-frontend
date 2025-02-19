bec3a46c042a28b1a9c3366189996c238213577c
Timothy Baba <timothybaba01@gmail.com>
2025-02-19 14:39:31 +0100

    Update on job posting and listing

---
+++ src/utils/emplymentTypes.ts
@@ -0,0 +1,9 @@
+export enum EmploymentType {
+    FULL_TIME = "Full Time",
+    PART_TIME = "Part Time",
+    CONTRACT = "Contract",
+    FREELANCE = "Freelance",
+    REMOTE = "Remote",
+    INTERNSHIP = "Internship",
+    TEMPORARY = "Temporary"
+  }
