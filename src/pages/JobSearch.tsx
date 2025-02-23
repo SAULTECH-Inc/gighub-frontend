4d267e51c0d41ce4cf3af4722b9d6615c9199f60
Shadrach Adamu <shadrachadamuul@gmail.com>
2025-02-23 21:10:41 +0100

    updaded

--- src/pages/JobSearch.tsx
+++ src/pages/JobSearch.tsx
@@ -11,12 +11,6 @@ import SalaryRangeSelector from "../components/common/SalaryRangeSelector.tsx";
 const JobSearch: React.FC = () => {
     const [isChecked, setIsChecked] = React.useState(false);
     const [selectedOption, setSelectedOption] = useState("option1");
-    const [, setSalaryData] = useState({
-        currency: "USD",
-        min: 3000,
-        max: 7000,
-        period: "Year",
-    });
     return <>
         <ApplicantNavBar navItems={applicantNavItems} navItemsMobile={applicantNavItemsMobile}
                          navbarItemsMap={applicantNavBarItemMap}/>
@@ -47,12 +41,11 @@ const JobSearch: React.FC = () => {
                 size={19}
                 color="#6E4AED"
             />
-            {/*<SalaryRangeSelector*/}
-            {/*    label="Desired Salary"*/}
-            {/*    min={30000}*/}
-            {/*    max={150000}*/}
-            {/*    onChange={(value) => console.log(value)}*/}
-            {/*/>*/}
+            <SalaryRangeSelector
+                baseMin={30000}
+                baseMax={150000}
+                onChange={(values) => console.log(values)}
+            />
         </div>
     </>
 }
