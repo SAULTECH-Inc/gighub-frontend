de96b8025d7402139ccef7bb8948876da3e76b5c
Shadrach Adamu <shadrachadamuul@gmail.com>
2025-03-30 01:51:39 +0100

    added more impls

--- src/pages/job/JobList.tsx
+++ src/pages/job/JobList.tsx
@@ -1,11 +1,10 @@
 import {FC} from "react";
-import TopNavBar from "../../components/layouts/TopNavBar.tsx";
 import JobFilterBar from "../jobList/JobFilterBar.tsx";
 import JobTable from "../jobList/JobTable.tsx";
-const EmployerProfile: FC = () => {
+const JobList: FC = () => {
     return (
         <div className="mx-auto">
-            <TopNavBar/>
+            {/*<TopNavBar/>*/}
             <div className="flex justify-center    pt-6 mx-auto gap-x-10 px-2 lg:px-5">
                 {/* Main Content */}
                 <div className=" rounded-[16px]  lg:w-[70%]  p-4 lg:p-8">
@@ -22,4 +21,4 @@ const EmployerProfile: FC = () => {
     );
 };
 
-export default EmployerProfile;
+export default JobList;
